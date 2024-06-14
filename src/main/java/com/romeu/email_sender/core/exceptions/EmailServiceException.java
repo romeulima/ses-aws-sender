e3e6758c9b893479da11cedee9173344a846685b
Romeu Lima <romeu.sousadp@gmail.com>
2024-06-14 12:12:52 -0300

    feat: create Email exception

---
+++ src/main/java/com/romeu/email_sender/core/exceptions/EmailServiceException.java
@@ -0,0 +1,8 @@
+package com.romeu.email_sender.core.exceptions;
+
+public class EmailServiceException extends RuntimeException {
+
+    public EmailServiceException(String message) {
+        super(message);
+    }
+}
